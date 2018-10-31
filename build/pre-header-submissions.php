5abfebee95c15693ffbbc32f7dee9650f385d9a9
unremarkablegarden <olle@unremarkablegarden.com>
2018-10-31 15:40:23 +0000

    stuff

--- build/pre-header-submissions.php
+++ build/pre-header-submissions.php
@@ -3,7 +3,7 @@
 			<img src="/wp-content/themes/rueckblende/build/assets/img/book/red_arrow_15x20.svg"/>
 		</div>
 		<div class="text">
-			Hier geht's zum Wettbewerb
+			<a href="/login/">Hier geht's zum Wettbewerb</a>
 		</div>
 		<div class="logo">
 			<img src="/wp-content/themes/rueckblende/build/assets/img/badges/r_2018@2x.png" alt="Rueckblende 2018"/>
