454fac7811cb093e254821c4968977b5b5922ed1
unremarkablegarden <olle@unremarkablegarden.com>
2019-01-16 15:36:17 +0000

    503

--- build/503.php
+++ build/503.php
@@ -8,20 +8,49 @@
 				height: 100vh;
 				width: 100vw;
 				overflow: hidden;
+				background-color: rgb(235,235,235)
 			}
 			body {
 				display: flex;
 				justify-content: center;
 				align-items: center;
+				flex-direction: column;
 			}
 			img {
 				width: 400px;
 				height: auto;
 			}
+			h1 {
+				font-family: 'Arial';
+				font-size: 30px;
+				margin-bottom: 2em;
+			}
+			#logotop {
+				position: fixed;
+				top: 3rem;
+				left: 3rem;
+				width: 250px;
+				height: auto;
+			}
+			@font-face{src:url(/wp-content/themes/rueckblende/build/assets/fonts/Bliss2L.otf);font-family:B;font-weight:300}
+			@font-face{src:url(/wp-content/themes/rueckblende/build/assets/fonts/Bliss2R.otf);font-family:B;font-weight:400}
+			@font-face{src:url(/wp-content/themes/rueckblende/build/assets/fonts/Bliss2M.otf);font-family:B;font-weight:500}
+			@font-face{src:url(/wp-content/themes/rueckblende/build/assets/fonts/Bliss2B.otf);font-family:B;font-weight:700}
+			body{font-family:B;font-weight:400; font-size: 18px;line-height: 2em; text-align: center;}
+			.text {
+				margin-top: 2em;
+			}
 		</style>
 	</head>
 	<body>
+		
 		<!-- <img src="https://rueckblende.rlp.de/wp-content/uploads/2018/01/logo_subline.svg" /> -->
-		<img src="http://165.227.164.168/wp-content/uploads/2018/10/Rueckblende_2018.svg" />
+		<img src="http://165.227.164.168/wp-content/uploads/2018/10/Rueckblende_2018.svg" id="logotop" />
+		<img src="http://165.227.164.168/wp-content/uploads/2018/03/soon_here@2x.png" style="width: 150px; height: auto;">
+		<div class='text'>
+		Die neue Rückblende 2018 steht kurz bevor —<br/>
+		nach der Preisverleihung am 28. Januar 2019<br/>
+		finden Sie hier ab 19 Uhr die neuen Webseiten.
+		</div>
 	</body>
 </html>
