c961f06ad5ba01d1c4827d8078894f6b9347b6e9
unremarkablegarden <olle@unremarkablegarden.com>
2021-01-22 16:16:49 +0000

    vote

--- build/pre-header-showroom.php
+++ build/pre-header-showroom.php
@@ -3,7 +3,7 @@
 			<img src="/wp-content/themes/rueckblende/build/assets/img/book/red_arrow_15x20.svg"/>
 		</div>
 		<div class="text">
-			<a href="/login/">Hier geht's zum Showroom</a>
+			<a href="/showroom/">Hier geht's zum Showroom</a>
 		</div>
 		<div class="logo">
 			<!-- <img src="/wp-content/themes/rueckblende/build/assets/img/badges/r_2018@2x.png" alt="Rueckblende 2018"/> -->
